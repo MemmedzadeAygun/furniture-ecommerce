419025cace726eede412bc6f47d72378984b3146
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2025-06-24 06:21:55 -0700

    project_create

---
+++ funiture-ecommerce/src/test/java/com/example/funiture_ecommerce/FunitureEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funiture_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunitureEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
