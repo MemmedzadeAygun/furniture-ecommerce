a84ca4b8bc415bafc80df883d59f0c00dbfca9d3
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2025-07-16 01:52:05 -0700

    Məhsulun silinməsi üçün APİ front-end ilə əlaqələndirildi

--- front-end/js/myProducts.js
+++ front-end/js/myProducts.js
@@ -1,5 +1,5 @@
 
-function loadOnProducts(){
+function loadOnProducts() {
     const token = localStorage.getItem('token');
 
     fetch('http://localhost:8086/products/getAll', {
@@ -8,16 +8,16 @@ function loadOnProducts(){
             'Authorization': `Bearer ${token}`
         }
     })
-    .then(async response => {
-        if (response.ok) {
-           let data = await response.json();
-            console.log(data);
+        .then(async response => {
+            if (response.ok) {
+                let data = await response.json();
+                console.log(data);
 
-            let products_tbody = document.getElementById("products-tbody");
+                let products_tbody = document.getElementById("products-tbody");
 
-            let tableContent = '';
-            data.products.forEach(element => {
-                tableContent += `
+                let tableContent = '';
+                data.products.forEach(element => {
+                    tableContent += `
                 <tr>
                 <td>${element.id}</td>
                 <td>${element.name}</td>
@@ -26,14 +26,15 @@ function loadOnProducts(){
                     <img src="${element.image}" style="width: 100px"; height: 80px; object-fit:cover; />
                 </td>
                 <td><button type="button" class="btn btn-primary edit-btn" data-id="${element.id}">Edit</td>
+                <td><button type="button" class="btn btn-danger delete-btn" data-id="${element.id}">Delete</td>
                 </tr>
                 
                 `
-            })
-            
-            products_tbody.innerHTML += tableContent;
-        }
-    })
+                })
+
+                products_tbody.innerHTML += tableContent;
+            }
+        })
 }
 
 loadOnProducts();
@@ -48,7 +49,32 @@ document.addEventListener('click', (e) => {
     if (e.target.classList.contains('edit-btn')) {
         const productId = e.target.getAttribute('data-id');
         console.log(productId);
-        
+
         window.location.href = `products.html?id=${productId}`;
+    }
+})
+
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete-btn')) {
+        const productId = e.target.getAttribute('data-id');
+        console.log(productId);
+
+        const token = localStorage.getItem('token');
+
+        if (confirm('Silmek istediyinize eminsinizmi?')) {
+            fetch(`http://localhost:8086/products/delete/${productId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    alert('Product delete successfully');
+                    e.target.closest('tr').remove();
+                }
+            })
+        }
+
     }
 })
