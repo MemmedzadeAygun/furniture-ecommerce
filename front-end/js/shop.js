52049bead7f87480bdcd0aa1f7b915c2369a9c5e
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2025-07-18 11:44:06 -0700

    Axtarış üçün APİ front ilə inteqrasiya edildi

--- front-end/js/shop.js
+++ front-end/js/shop.js
@@ -56,3 +56,61 @@ function showProducts(){
 }
 
 showProducts();
+
+function searchProduct(){
+    const token = localStorage.getItem('token');
+
+    const query = document.getElementById('searchInput').value;
+
+    fetch(`http://localhost:8086/products/search?query=${query}`, {
+        method: 'GET',
+        headers:{
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(async response => {
+        let data = await response.json();
+        console.log(data);
+
+        let cards = document.querySelector('.cards');
+        cards.innerHTML = '';
+        
+        data.forEach(element => {
+
+                let cardsDiv = document.querySelector('.cards');
+
+                let cardDiv = document.createElement('div');
+                cardDiv.classList.add('card');
+                
+                let imgDiv = document.createElement('div');
+
+                let image = document.createElement('img');
+                image.src = element.image;
+
+                let h5 = document.createElement('h5');
+                h5.textContent = element.name;
+
+                let p = document.createElement('p');
+                p.textContent = element.price + " AZN";
+
+                let addToCardBtn = document.createElement('button');
+                addToCardBtn.textContent = 'add to card';
+                addToCardBtn.style.backgroundColor = 'black';
+                addToCardBtn.style.color = 'white';
+                addToCardBtn.style.border = 'none';
+                addToCardBtn.style.width = '100%';
+                addToCardBtn.style.padding = '4px';
+                addToCardBtn.style.cursor = 'pointer';
+
+        
+
+                imgDiv.append(image);
+                cardDiv.append(imgDiv);
+                cardDiv.append(h5);
+                cardDiv.append(p);
+                cardDiv.append(addToCardBtn);
+
+                cardsDiv.append(cardDiv);
+            });
+    })
+}
