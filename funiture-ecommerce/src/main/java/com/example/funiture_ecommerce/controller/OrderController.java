f5fcf6fa46dfc04e9bb552cbd3ebb568aa87adab
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2025-08-04 06:39:03 -0700

    Backenddə məhsul sifarişi üçün validasiya əlavə olundu

--- funiture-ecommerce/src/main/java/com/example/funiture_ecommerce/controller/OrderController.java
+++ funiture-ecommerce/src/main/java/com/example/funiture_ecommerce/controller/OrderController.java
@@ -2,14 +2,18 @@ package com.example.funiture_ecommerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.funiture_ecommerce.exception.OurRuntimeException;
 import com.example.funiture_ecommerce.requestDto.OrderRequestDto;
 import com.example.funiture_ecommerce.service.OrderService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(path = "/orders")
@@ -20,7 +24,10 @@ public class OrderController {
 	private OrderService orderService;
 	
 	@PostMapping(path = "/add")
-	public ResponseEntity<String> order(@RequestBody OrderRequestDto dto){
+	public ResponseEntity<String> order(@Valid @RequestBody OrderRequestDto dto, BindingResult br){
+		if (br.hasErrors()) {
+			throw new OurRuntimeException(br, "melumatlarin tamliginda problem var");
+		}
 		orderService.order(dto);
 		return ResponseEntity.ok("Order was created successfully");
 	}
